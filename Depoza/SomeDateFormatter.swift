3f2a8d23461d9f68da3111411639ee269e1d1f44
Ivelin Davidov <ivelin.davidov@tumba.solutions>
2017-10-19 23:07:20 +0300

    Added some Swift code so the project always embeds swift standard libraries.

---
+++ Depoza/SomeDateFormatter.swift
@@ -0,0 +1,15 @@
+//
+//  SomeDateFormatter.swift
+//  Depoza
+//
+//  Created by Ivelin Davidov on 10/18/17.
+//  Copyright © 2017 Ivan Magda. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreImage
+
+class SomeDateFormatter {
+    
+}
